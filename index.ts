b709b81d98fd6e7c50f97e9d38969a83c26ba590
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-09-02 12:34:10 +0200

    post method

--- index.ts
+++ index.ts
@@ -20,19 +20,77 @@ app.get("/", (req, res) => {
 })
 
 app.get("/houses", (req, res) => {
-   let housesToSend=houses.map(house=>{
-    let residentsOfHouse=residents.filter(resident=> resident.houseId===house.id)
-    return{...house, residents: residentsOfHouse}
-   })
-   res.send(housesToSend)
+    let housesToSend = houses.map(house => {
+        let residentsOfHouse = residents.filter(resident => resident.houseId === house.id)
+        return { ...house, residents: residentsOfHouse }
+    })
+    res.send(housesToSend)
 })
 
 app.get("/residents", (req, res) => {
-  let residentsToSend=residents.map(resident=>{
-    let house=houses.find(house=>house.id===resident.houseId)
-    return{...resident, house}
-  })
-  res.send(residentsToSend)
+    let residentsToSend = residents.map(resident => {
+        let house = houses.find(house => house.id === resident.houseId)
+        return { ...resident, house }
+    })
+    res.send(residentsToSend)
+})
+
+app.post('/houses', (req, res) => {
+    let errors: string[] = []
+    if (typeof req.body.address !== "string") {
+        errors.push("Please enter a valid address")
+    }
+
+    if (typeof req.body.type !== "string") {
+        errors.push("Please enter a valid house type")
+    }
+
+    if (errors.length === 0) {
+        const newHouse = {
+            id: houses[houses.length - 1].id + 1,
+            address: req.body.address,
+            type: req.body.type
+        }
+        houses.push(newHouse)
+        res.send(newHouse)
+    }
+    else{
+        res.status(400).send({errors: errors})
+    }
+    
+
+})
+
+app.post('/residents', (req, res)=>{
+    let errors:string[]=[]
+
+    if(typeof req.body.name !== "string"){
+        errors.push("Please enter a valid name")
+    }
+    if(typeof req.body.age !== "number"){
+        errors.push("Please enter a valid age")
+    }
+    if(typeof req.body.gender !== "string"){
+        errors.push("Please enter a valid gender")
+    }
+    if(typeof req.body.houseId !== "number"){
+        errors.push("Please enter a valid house id")
+    }
+
+    if(errors.length===0){
+        const newResident={
+            id: residents[residents.length-1].id +1,
+            name: req.body.name,
+            age: req.body.age,
+            gender: req.body.gender,
+            houseId: req.body.houseId
+        }
+        residents.push(newResident)
+        res.send(newResident)
+    }
+    else{
+        res.status(400).send({errors: errors})
+    }
 })
 
 app.listen(port)
