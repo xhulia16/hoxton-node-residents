61df6c0daded62ace02fe12520fa8a2b1f4371ad
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-09-02 13:47:55 +0200

    commit

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors  from 'cors'
 import { houses, residents } from './data'
 
 const app = express()
 const port = 5000
 app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send(`
