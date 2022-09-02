5dd3006d4ed66022136b20f341b8a12930eab18d
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-09-02 11:29:33 +0200

    get houses and residents

--- index.ts
+++ index.ts
@@ -1,5 +1,38 @@
 import express from 'express'
-import {houses, residents} from './data'
-const app=express()
-const port=5000
+import { houses, residents } from './data'
+
+const app = express()
+const port = 5000
 app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send(`
+<h1>Available resources:<h1>
+<ul>
+<li>
+<a href="/houses"> Houses </a>
+</li>
+<li>
+<a href="/residents"> Residents </a>
+</li>
+</ul>
+`)
+})
+
+app.get("/houses", (req, res) => {
+   let housesToSend=houses.map(house=>{
+    let residentsOfHouse=residents.filter(resident=> resident.houseId===house.id)
+    return{...house, residents: residentsOfHouse}
+   })
+   res.send(housesToSend)
+})
+
+app.get("/residents", (req, res) => {
+  let residentsToSend=residents.map(resident=>{
+    let house=houses.find(house=>house.id===resident.houseId)
+    return{...resident, house}
+  })
+  res.send(residentsToSend)
+})
+
+app.listen(port)
